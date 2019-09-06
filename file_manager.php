201a7929e174a0624c806e9cab4aee220ad92d80
felixhu <felixhu@ip-172-31-21-201.us-east-2.compute.internal>
2019-09-06 09:38:33 -0500

    fieo

--- file_manager.php
+++ file_manager.php
@@ -2,7 +2,7 @@
 session_start();
 
 if(isset($_SESSION['username'])){
-    $name=$_SESSION['username'];
+    $name=(string)$_SESSION['username'];
     printf("<p>Welcome, %s</p>", $name);
 } else {
    printf("Could not identify the user, did you logout?");
