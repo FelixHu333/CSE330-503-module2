0488808485a64b880ba45cf4b0173434427ca49b
felixhu <felixhu@ip-172-31-21-201.us-east-2.compute.internal>
2019-09-06 13:27:46 -0500

    added table of files

--- file_manager.php
+++ file_manager.php
@@ -3,8 +3,28 @@ session_start();
 
 if(isset($_SESSION['username'])){
     $name=(string)$_SESSION['username'];
-    printf("<p>Welcome, %s</p>", $name);
+    # VALIDATION CODE TAKEN FROM https://classes.engineering.wustl.edu/cse330/index.php?title=PHP#Sending_a_File_to_the_Browser 
+    if( !preg_match('/^[\w_\-]+$/', $name) ){
+	echo "Invalid username";
+	exit;
+    }
+    $full_path = sprintf("/srv/uploads/%s/", $name);
+    # end code snippet from wiki
+    # code taken from https://stackoverflow.com/questions/15774669/list-all-files-in-one-directory-php
+    $files = array_diff(scandir($full_path), array('.', '..'));
+    # end code taken from stackoverflow
+    
 } else {
    printf("Could not identify the user, did you logout?");
 }
 ?>
+
+<table>
+<?php
+foreach($files as $fil){
+    echo sprintf("<tr><td valign='center'>%s</td>\n<td valign='center'><form><input type=submit value='view'></form></td><td valign='center'><form><input type=submit value='delete'></form></td></tr>",
+	htmlentities($fil)
+    );
+}
+?>
+</table>
--- view_file.php
+++ view_file.php
@@ -23,5 +23,5 @@ $finfo = new finfo(FILEINFO_MIME_TYPE);
 $mime = $finfo->file($full_path);
 header("Content-Type: ".$mime);
 readfile($full_path);
-
+# end code taken from wiki
 ?>
