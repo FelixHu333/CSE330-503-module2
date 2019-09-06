7cc325c6d2c1f2691acc0613f2ecede979b44641
felixhu <felixhu@ip-172-31-21-201.us-east-2.compute.internal>
2019-09-06 10:24:46 -0500

    added file viewer

---
+++ view_file.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+if(isset($_SESSION['username'])){
+    $name=(string)$_SESSION['username'];
+    #iprintf("<p>Welcome, %s</p>", $name);
+} else {
+   printf("Could not identify the user, did you logout?");
+}
+
+$filename = $_GET['file'];
+
+# VALIDATION CODE TAKEN FROM https://classes.engineering.wustl.edu/cse330/index.php?title=PHP#Sending_a_File_to_the_Browser
+if( !preg_match('/^[\w_\.\-]+$/', $filename) ){
+	echo "Invalid filename";
+	exit;
+}
+if( !preg_match('/^[\w_\-]+$/', $name) ){
+	echo "Invalid username";
+	exit;
+}
+$full_path = sprintf("/srv/uploads/%s/%s", $name, $filename);
+$finfo = new finfo(FILEINFO_MIME_TYPE);
+$mime = $finfo->file($full_path);
+header("Content-Type: ".$mime);
+readfile($full_path);
+
+?>
